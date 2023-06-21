3164cb937902c4b5f539848056299833081b84cd
torcuata22 <marilynjmarquez@gmail.com>
2023-06-21 14:09:21 -0400

    stock api working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,15 @@
 class HomeController < ApplicationController
   require 'stock_quote'
   def index
+    if params[:ticker] == ""
+      @nothing = "You forgot to enter a symbol"
+    elsif params[:ticker]
     StockQuote::Stock.new(api_key: 'sk_c21a00c6fe03437481d9371e280d2c04')
-    begin
-      @stock = StockQuote::Stock.quote('FB')
-      @stock_div = StockQuote::Stock.dividends('FB')
-    rescue StandardError => e
-      @error_message = "Error occurred: #{e.message}"
+      begin
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue RuntimeError => e
+      @error = "There was an Error retrieving stock data: #{e.message}"
+      end
     end
   end
 
