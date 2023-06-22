0ce948b23f4d322df43a2eb9843d477b771f19d3
torcuata22 <marilynjmarquez@gmail.com>
2023-06-22 16:23:15 -0400

    authentication and authorization, associations

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,7 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[ show edit update destroy ]
-
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   # GET /stocks or /stocks.json
   def index
     @stocks = Stock.all
@@ -55,6 +56,12 @@ class StocksController < ApplicationController
       format.html { redirect_to stocks_url, notice: "Stock was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @ticker = current_user.stocks.find_by(id: params[:id])
+    redirect_to stocks_path, notice: "Not Authorized to edit this stock" if @ticker.nil?
+
   end
 
   private
