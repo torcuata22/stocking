7c7ffd4020b48b0d631b055a309a0f3fc25708a9
torcuata22 <marilynjmarquez@gmail.com>
2023-06-22 13:19:41 -0400

    authentication done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/about'
   post "/" => 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
